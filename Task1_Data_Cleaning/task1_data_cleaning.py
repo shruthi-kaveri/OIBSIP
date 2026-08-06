f0c11d7aacb20dc8aea23f315fdfffec317ffc49
shruthi-kaveri <shruthikaveri.05@gmail.com>
2026-08-06 09:36:13 +0530

    Update task1_data_cleaning.py

--- Task1_Data_Cleaning/task1_data_cleaning.py
+++ Task1_Data_Cleaning/task1_data_cleaning.py
@@ -1,1 +1,20 @@
+import pandas as pd
 
+# Load the Titanic dataset
+df = pd.read_csv("train.csv")
+
+# Display first 5 rows
+print("First 5 Rows:")
+print(df.head())
+
+# Display dataset information
+print("\nDataset Information:")
+print(df.info())
+
+# Display missing values
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+# Display statistical summary
+print("\nStatistical Summary:")
+print(df.describe())
